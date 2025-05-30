f276315fc79c0d485819873d88b7530bef269cd3
jeethmshah <jeethmshah@gmail.com>
2025-05-30 23:24:59 +0530

    Fix CORS origin using CLIENT_URL environment variable

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,6 +9,8 @@ import { app, server } from "./lib/socket.js";
 import path from "path";
 import { fileURLToPath } from "url";
 
+
+
 // Setup for __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
