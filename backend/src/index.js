68b2d9bc07a2e1b8ba7b64e0844c2a487901e70a
jeethmshah <jeethmshah@gmail.com>
2025-05-30 22:00:26 +0530

    Fix __dirname usage in index.js for ESM

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,33 +7,39 @@ import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
 import path from "path";
+import { fileURLToPath } from "url";
 
-dotenv.config()
+// Setup for __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+dotenv.config();
 
 const PORT = process.env.PORT;
-const__dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: "http://localhost:5173",
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Change this to your frontend domain on production
     credentials: true,
-
-})
+  })
 );
-app.use("/api/messages",messageRoutes);
 
+app.use("/api/messages", messageRoutes);
 app.use("/api/auth", authRoutes);
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+// Serve static files in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req,res) => {
-        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 server.listen(PORT, () => {
-    console.log("Server is running on PORT:" + PORT);
-    connectDB();
+  console.log("Server is running on PORT: " + PORT);
+  connectDB();
 });
