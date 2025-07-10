ba61c39da70e43edecc7038abf0a8fe755193e3f
jeethmshah <jeethmshah@gmail.com>
2025-07-10 21:58:47 +0530

    update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,13 +33,13 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/auth", authRoutes);
 
 // Serve static files in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 server.listen(PORT, () => {
   console.log("Server is running on PORT: " + PORT);
